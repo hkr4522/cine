1a00c1342bb32f4944518ad240fdb903b6811822
hkr4522 <hkr379628@gmail.com>
2025-09-14 23:41:22 +0530

    Update constants.ts

--- src/utils/config/constants.ts
+++ src/utils/config/constants.ts
@@ -1,7 +1,7 @@
 
 const TMDB = {
   API_KEY: import.meta.env.VITE_TMDB_API_KEY as string,
-  BASE_URL: 'https://flickystream.co/proxy/?url=api.themoviedb.org/3',
+  BASE_URL: 'https://api.themoviedb.org/3',
   IMAGE_BASE_URL: 'https://image.tmdb.org/t/p',
   posterSizes: {
     small: 'w185',
